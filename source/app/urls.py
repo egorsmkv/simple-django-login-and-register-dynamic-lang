bb48d3ea48d7f94334d8a486b78032bff0830da2
Yehor Smoliakov <yehors+gh@ukr.net>
2022-11-20 19:23:57 +0200

    Init

---
+++ source/app/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+
+from main.views import IndexPageView, ChangeLanguageView
+
+urlpatterns = i18n_patterns(
+    path('admin/', admin.site.urls),
+
+    path('', IndexPageView.as_view(), name='index'),
+
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('language/', ChangeLanguageView.as_view(), name='change_language'),
+
+    path('accounts/', include('accounts.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
